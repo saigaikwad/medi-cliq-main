cdb07894eee5a34455da3ba7fc7c114724fd475f
sai gaikwad <saigaikwad936@gmail.com>
2025-02-25 11:50:38 +0530

    material ui to doctor login and signup

--- app/models/prescription.rb
+++ app/models/prescription.rb
@@ -9,14 +9,22 @@ class Prescription < ApplicationRecord
 
   def generate_qr_code
     qr_data = {
-      patient_name: patient.name,
       medicine_id: medicine.id,
       quantity: prescription_quantity
     }.to_json
 
-    qr = RQRCode::QRCode.new(qr_data)
-    qr_svg = qr.as_svg(module_size: 6)
+    qr_code = RQRCode::QRCode.new(qr_data).as_svg(module_size: 6)
 
-    Qr.create(prescription_id: id, qr_code: qr_svg)
+    created_qr = Qr.create(prescription_id: id, qr_code: qr_code)
+
+    # Now use the created Qr record
+    qr_data = {
+      qr_id: created_qr.id,
+      medicine_id: medicine.id,
+      quantity: prescription_quantity
+    }.to_json
+
+    # Update the QR code with the proper `qr_id`
+    created_qr.update(qr_code: RQRCode::QRCode.new(qr_data).as_svg(module_size: 6))
   end
 end
