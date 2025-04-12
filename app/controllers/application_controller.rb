c90285db54b4964b29f66fce1b990b93e063613b
sai gaikwad <saigaikwad936@gmail.com>
2025-04-12 18:00:38 +0530

    ui changes of admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_admin2!, if: -> { request.path.start_with?('/admin2') }
+  before_action :authenticate_admin_user!, if: -> { request.path.start_with?('/admin') }
 
   def authenticate_user!
-    if admin2_signed_in?
-      authenticate_admin2!
+    if admin_user_signed_in?
+      authenticate_admin_user!
     elsif doctor_signed_in?
       authenticate_doctor!
     elsif patient_signed_in?
@@ -14,8 +14,8 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    if resource.is_a?(Admin2)
-      admin2_root_path
+    if resource.is_a?(Admin)
+      admin_root_path
     else
       super
     end
